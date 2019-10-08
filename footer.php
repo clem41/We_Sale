861daffad73522edd142c29a032edf296631e2d0
XingchenLI <lixingchen060@gmail.com>
2019-10-08 14:48:27 +0200

    footer with css

---
+++ footer.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<style type="text/css">
+div.container
+{
+width:100%;
+margin:0px;
+line-height:150%;
+}
+div.header
+{
+color:rgb(255,255,255);
+background-color:rgb(0,0,0);
+}
+div.footer
+{
+position:absolute;
+top:1200px;
+padding-bottom:20px;
+padding-left:50%;	
+padding-right:50%;
+padding-top:20px;
+line-height:50px;
+color:rgb(255,255,255);
+background-color:rgb(180,100,100);
+border:0;
+white-space: nowrap;
+}
+h1.header
+{
+padding:0;
+margin:0;
+}
+div.content
+{
+
+}
+</style>
+</head>
+<body>
+<div class="container">
+<div class="header">
+<div class="content">
+<div class="footer">
+<p><center>Welcome to My Sale!</center></p>
+<p><center>Copyright © 2019</center></p>
+</div>
+</body>
+</html>
