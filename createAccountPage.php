40375f010bd38d2f69a8c7e180817e8fdc0e579d
julien <julien.lecoeuche@isne>
2019-10-10 12:30:51 +0200

    account_html

---
+++ createAccountPage.php
@@ -0,0 +1,35 @@
+<!<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		createAccountPage
+	</title>
+	<style type="text/css">
+	label{
+		
+		display: block;
+		width :150 px;
+		
+	}
+	</style>
+
+</head>
+	
+<body>
+
+ 	<form> 
+ 		<label> <?php echo "lastName" ?> </label> <input type="text" name="lastName">
+	 	<br>
+	 	<label>	<?php echo "name" ?> </label> <input type="text" name="name">
+	 	<br>
+	 	<label>	<?php echo "e-mail" ?> </label> <input type="text" name="e-mail">
+	 	<br>
+	 	<label> <?php echo "mot de passe" ?></label> <input type="text" name="mot de passe">
+	 	<br>
+	 	<label> <?php echo "confirmer mot de passe" ?></label><input type="text" name="confirmer mot de passe"><br>
+	 	<input type="button" value =" valider ">
+ 		
+ 	</form>
+
+</body>
+</html>
