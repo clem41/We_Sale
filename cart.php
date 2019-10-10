46a41e2c8259bade8389aa828c158ffbc05b7405
Clotilde <clotilde.zenone@isen.yncrea.fr>
2019-10-10 22:29:47 +0200

    first version of the cart page

---
+++ cart.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<title>We Sale ! Your Cart</title>
+<head>
+	<?php //include header
+	?>
+</head>
+<body>
+	<p>Your cart is empty</p>
+</body>
+
+<footer>
+	<?php //include footer
+	?>
+</footer>
