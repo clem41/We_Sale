8734146bdaead15afc88975c75e0ed89d2aec9d0
julien <julien.lecoeuche@isne>
2019-10-24 17:43:09 +0200

    addToCart beginning

--- products.php
+++ products.php
@@ -6,13 +6,37 @@
 	
 </head>
 <body>
+	<?php
+		$bdd = new PDO('mysql:host=localhost;dbname=dump','root','');
+	?>
 	<?php include'header.php';
 	?>
+	<form id= "cart_functions.php">
+		<div class="article">
+			<?php
+				$response = $bdd->query('SELECT * FROM products' );
+				
+				
 
-	<div class="article">
-		<img id="mainImg" src="cereales.jpg">
-		<p>Un produit à la con</p>
-	</div>
+				while ($article1 = $response->fetch()) 
+				{
+				
+				?>
+					
+					<p>
+						<?php echo $article1['name'];?><br>
+						<img id="mainImg" src="cereales.jpg">
+						description de l'objet : <?php echo $article1['description']; ?> <br>
+						son prix est de : <?php echo $article1['unit_price'];?><br>
+						<input type="submit" value=" Ajouter au panier">
+					</p>
+				<?php
+				}
+
+				$response->closeCursor();
+				?>
+		</div>
+
 </body>
 
 <footer>
