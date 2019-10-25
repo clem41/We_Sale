207438927281b5f792124a56f3776d716046e942
Clotilde <clotilde.zenone@isen.yncrea.fr>
2019-10-25 17:03:47 +0200

    Database : function can be add by everyone

--- database.php
+++ database.php
@@ -1,16 +1,32 @@
 <?php
 
-function getAllRanges()
+function getOrderByType($orderType)
 {
-	$query  = "select * from ranges";
-	return executeQuery($query,null)
+    $params = array('orderType' => $orderType);
+    $query = '
+        select o.*
+          from orders o
+          where o.Type = :orderType
+    ';
+    return executeQuery($query, $params);
 
+    
+    function executeQuery($query, $params)
+{
+    //FIXME: change dbname by your own dbname
+    $bdd = new PDO('mysql:host=localhost;dbname=technoweb', 'root') ;
+    try {
+        $res = $bdd->prepare($query);
+        $res->execute($params);
+
+        $datas = $res->fetchAll();
+
+        $res->closeCursor();
+
+        return $datas;
+    } catch (PDOException $e) {
+        var_dump($e);
+    }
+}
 }
 
-function getRangeById($rangeId)
-{
-	$params = array('rangeId' =>$rangeId);
-	$query = "select r.* from ranges r where r.id = :rangeId";
-	return executeQuery($query,$params);
-}
-function g
