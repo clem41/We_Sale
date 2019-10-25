dca1ecadea4bb04d891831de26d658871a2461b8
pei.zhang <1181466873@qq.com>
2019-10-25 16:16:56 +0200

    add the sort

--- cart.php
+++ cart.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html>
+<style>
+.category{
+	display:block;
+	}
+</style>
 <title>We Sale ! Your Cart</title>
 <head>
 	<link rel="stylesheet" href="main.css" />
 </head>
 <body>
+<div class="category">
 		<?php include'header.php';
 	?>
+	</div>
+	<div class="category">
 	<p>Your cart is empty</p>
+	</div>
 		<?php include'footer.php';
 	?>
 </body>
--- resultdisplay.php
+++ resultdisplay.php
@@ -9,13 +9,7 @@ div.img {
     float: left;
     width: 180px;
 }
-div.header{
-	display:block;
-	}
 display.category{
-	display:block;
-	}
-div.nav{
 	display:block;
 	}
 div.img:hover {
@@ -60,25 +54,53 @@ li.display a {
 li.display a:hover {
     background-color: #111;
 }
+.button {
+	display:block;
+	margin: 0 auto;
+    background-color: #e7e7e7;
+    color: black;
+    border: none;
+	padding: 4px 8px;
+    text-align: center;
+    text-decoration: none;
+    display:block;
+    font-size: 12px;
+    cursor: pointer;
+}
+a.button{
+	display:inline;
+    text-align: center;
+    text-decoration: none;
+}
 </style>
 </head>
 <body>
 <div class="category">
 <?php include 'header.php'?>
+<?php $name=$_GET[name];?>
 </div>
 <div class="category">
 <ul class="display">
-  <li class="display"><a class="active" href="main.html">Back to main</a></li>
-  <li class="display"><a href="Pricelow">Price:low to high</a></li>
-  <li class="display"><a href="Pricehigh">Price:high to low</a></li>
+  <li class="display"><a class="active" href="main.php">Back to main</a></li>
+  <li class="display"><a href="resultdisplay.php?name=<?php echo $name?>&sort=low">Price:low to high</a></li>
+  <li class="display"><a href="resultdisplay.php?name=<?php echo $name?>&sort=hight">Price:high to low</a></li>
   <li class="display"><a href="Newest">Newest Arrival</a></li>
 </ul>
 </div>
 <div>
 <?php
-$name=$_GET[name]; 
+$name=$_GET[name];
 $bdd = new PDO('mysql:host=localhost;dbname=dump','root','root');
-   $res = $bdd->prepare("select * from products where name like '%$name%'");
+$flag=$_GET[sort];
+   if($flag=="low"){
+   $res = $bdd->prepare("select * from products where name like '%$name%' order by unit_price");
+   }
+   elseif($flag=="hight"){
+   $res = $bdd->prepare("select * from products where name like '%$name%' order by unit_price desc");
+   	}
+   	else{
+   		$res = $bdd->prepare("select * from products where name like '%$name%'");
+   		}
         $res->execute();
 		$products = $res->fetchAll();
 	  	$num=count($products);
@@ -98,8 +120,8 @@ $bdd = new PDO('mysql:host=localhost;dbname=dump','root','root');
 		echo $product['name'];
 		echo "<br>";
 		echo $product['unit_price'];
-		
       ?>
+<button class="button"><a class="button" href="cart.php">add to cart</a></button>
 	</div>
 	
 	   </div>
