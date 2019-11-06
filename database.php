10b53a444abbf53d9ce47bf0799298f946c2d1a4
pei.zhang <1181466873@qq.com>
2019-11-06 11:01:57 +0100

    add the fuzzy search

--- database.php
+++ database.php
@@ -6,9 +6,9 @@
 {
     //FIXME: change dbname by your own dbname
 	//uncomment for mac emvironmemt
-	//$bdd = new PDO('mysql:host=localhost;dbname=bd_project','root','root');
+	$bdd = new PDO('mysql:host=localhost;dbname=bd_project','root','root');
 	//uncomment for windows environnment
-    $bdd = new PDO('mysql:host=localhost;dbname=bd_projet', 'root');
+    //$bdd = new PDO('mysql:host=localhost;dbname=bd_projet', 'root');
     try {
         $res = $bdd->prepare($query);
         $res->execute($params);
@@ -67,7 +67,7 @@ function getProductNameById($productId)
 function getProductByNameGivenBySearch($string)
 {
 	$params = array('string'=> $string);
-	$query='select * from products where name like :string';
+	$query="select * from products where name like '$string%'";
 	return executeQuery($query,$params);
 }
 
