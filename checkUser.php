a0ba68331514236abc813a6a635d1bbc90149968
pei.zhang <1181466873@qq.com>
2019-11-08 18:23:38 +0100

    modify the checkUser.php

--- checkUser.php
+++ checkUser.php
@@ -1,14 +1,41 @@
-<?php include 'header.php' ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>displayForm</title>
+<link rel="stylesheet" href="display.css" />
+</head>
+<body>
+<div class="category">
+<?php include 'header.php'?>
+<?php $name=$_GET['name'];?>
+</div>
+<div class="category">
+<ul class="display">
+  <li class="display"><a class="active" href="main.php">Start to shopping</a></li>
+  <li class="display"><a class="active" href="createAccountPage.php">creat a new account</a></li>
+</ul>
+</div>
 <?php
   $name=$_POST['username'];
   $psw=$_POST['psw'];
-  $password=logIn($name);
-  var_dump($password);
-  echo $password[0]['PASSWORD'];
-  if($psw==$password[0]['PASSWORD']){
-  	 echo 1;
+  if($name!=''){
+  	$userfound=selectUserByUsername($name);
+	if($userfound==NULL){
+		echo 'this user dosent exist';
+		
+	}
+	else{
+			$password=logIn($name);
+  if($psw==$password[0]['password']){
+  	 echo 'sucsses';
   	}
   	else{
-  	echo 2;	
-  		}
+  	echo 'fail';	
+  		}}}
+  else{
+  	echo 'please input the both name and password';
+  	}
 ?>
+<?php include 'footer.php'?>
+</body>
+</html>
