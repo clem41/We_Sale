a8219d9d658e4590ad6c7f10e8bf042ae8d55320
XingchenLI <lixingchen060@gmail.com>
2019-11-08 17:38:56 +0100

    Execute actions (insert/update into database)  &  createAccountPage.php
    add a path to get the data from createAccountPage.php and insert them into the database from insert_update_into_db.php.

---
+++ insert_update_into_db.php
@@ -0,0 +1,52 @@
+<?php
+	include_once 'database.php';
+	$bdd = new PDO('mysql:host=localhost;dbname=bd_project', 'root');
+	//$con = mysql_connect("localhost","root","");
+	//if (!$con)
+	if(!$bdd)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+
+	//mysql_select_db("bd_project", $con);
+
+	$sql="INSERT INTO `users` (`id`, `username`, `email`, `password`, 
+	`billing_adress_id`, `delivery_adress_id`, `created_at`, `updated_at`) 
+	VALUES
+	('$_POST[name]','$_POST[email]','$_POST[password]')";
+	
+	/*
+	echo $_POST["name"];
+	echo $_POST["email"];
+	echo $_POST["password"];
+	*/
+	
+	//if (!mysql_query($sql,$con))
+	/*
+	if (!mysql_query($sql,$bdd))
+	{
+		die('Error: ' . mysql_error());
+	}
+	echo "1 record added";
+	
+    mysql_close($bdd);
+	*/
+	//mysql_close($con);
+	
+?>
+<html>
+<head>
+	<title>Success!</title>
+	
+	<link rel="stylesheet" href="productCSS.css" />
+	<link rel="stylesheet" href="display.css" />
+	
+</head>
+<body>
+<div class="category">
+<ul class="display">
+<li class="display"><a class="active" href="main.php">Congratulations! You are our new client now.</a></li>
+</ul>
+</div>
+</body>
+</html>
