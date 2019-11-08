4229def9aa1f54b965b52c193acecda81866c4dd
Clemence <clemence.hien@isen.yncrea.fr>
2019-11-08 14:15:22 +0100

    skeleton ended 2

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 //TODO start session
 
 //TODO include bd_projet.php file
+?>
 
 <?php
 //TODO assign database connexion into $database variable
 
 //TODO include checkUser.php file
+?>
 
 <?php
 //TODO (in the next step) control user access
@@ -14,6 +16,7 @@
 //TODO get page parameter ($_GET['page'] or $_POST['page']) and assign it into $page variable
 
 //if 'action/'.$page'.php' exists then include it (use file_exists($filename) function)
+?>
 
 <?php
 
@@ -27,6 +30,7 @@
 // TODO insert the start html envelope (<html><head>....</head><body>
 
 // TODO using $page decide to include header.php
+?>
 
 <?php
 
@@ -34,6 +38,7 @@
 
 //TODO if 'view/'.$page'.php' exists then include it (use file_exists($filename) function)
 //           else include 'view/main.php' (it has to exist)
+?>
 
 <?php
 
@@ -47,5 +52,6 @@
 // add view display possibly using data from database
 
 // TODO insert the end html envelope (</body></html>)
+?>
 
 ?>
