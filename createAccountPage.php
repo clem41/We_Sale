4878ebfacc8751a58c6265caf818f108a4a1f915
Clotilde <clotilde.zenone@isen.yncrea.fr>
2019-11-22 16:10:32 +0100

    modified createaccoundpage

--- createAccountPage.php
+++ createAccountPage.php
@@ -7,16 +7,38 @@
 </head>
 <body>
 <?php
-if(!isset($_POST['name'])){
-$flag=0;}
-else if(isset($_POST['name'])){
-$flag=1;
+
+if (isset($_POST['submit'])){
+	$userfound=selectUserByUsername($_POST['name']);
+     $emailfound=selectUserByUsername($_POST['email']);
+	if(($_POST['name']=='')||($_POST['email']=='')||($_POST['password']=='')||($_POST['confirmpassword']=='')){
+  	?><br><?php 
+		  	echo"<br>";
+	 echo"Please, make sure that all the parts of the form are filled";
 	}
-?>
-<?php
-switch($flag){
-	case 0:
-	{?>
+ 
+  else if($_POST['password']!=$_POST['confirmpassword']){
+  	?><br><?php  	
+  	echo"<br>";
+  	echo "The password and the password confirmation don't match";
+  }
+   else if($userfound!=NULL||$emailfound!=NULL){
+  	?><br><?php 
+  	echo"<br>";  
+echo 'this user already exists';
+			 
+	    }
+else{
+	 	addUserToDatabase($_POST['name'],$_POST['email'],$_POST['password']);
+  echo '<div class="category">
+	    <ul class="display">
+	    <li class="display"><a class="active" href="index.php?page=main">Congratulations! You are our new client now. Click here to start your shopping.</a></li>
+	    </ul>
+	    </div>';
+	 	}
+  	}
+  	?>
+	<br>
  	<form action = "index.php?page=account" method = "post">
  		<p class="inscritpion">
 			 
@@ -36,58 +58,9 @@ switch($flag){
 		 	<label> confirm your password </label>
 		 	<br>
 		 	<input type="password" name="confirmpassword"><br><br>
-		    <input type="submit">
+		    <input name = 'submit' type="submit">
  		
 			</p>
  	</form>
-<?php }break;
-  case 1:
-{?>
-<?php 
-echo "<br>";
-echo "<br>";
-  $name=$_POST['name'];
-  $email=$_POST['email'];
-  $psw1=$_POST['password'];
-  $psw2=$_POST['confirmpassword'];
-  if($name==""||$email==""){
-  		echo '<div class="category">
-	    <ul class="display">
-       <li class="display"><a class="active" href="index.php?page=account">Please click here to recreate the user</a></li>
-	    </ul>
-	    </div>';
-  	echo 'Please make sure that the username and password are not empty.';
-  	}
-  else if($psw2!=$psw1){
-  		echo '<div class="category">
-	    <ul class="display">
-       <li class="display"><a class="active" href="index.php?page=account">Please click here to recreate the user</a></li>
-	    </ul>
-	    </div>';
-  	echo 'please  make the two password the same';
-  	}
-  else{
-     $userfound=selectUserByUsername($name);
-     $emailfound=selectUserByUsername($email);
-     if($userfound!=NULL||$emailfound!=NULL){
-	 	echo '<div class="category">
-	    <ul class="display">
-       <li class="display"><a class="active" href="index.php?page=account">Please click here to recreate the user</a></li>
-	    </ul>
-	    </div>';
-	     echo 'this user has aready exist';
-			 
-	    }
-	 else{
-	 	addUserToDatabase($name,$email,$psw1);
-  echo '<div class="category">
-	    <ul class="display">
-	    <li class="display"><a class="active" href="index.php?page=main">Congratulations! You are our new client now. Click here to start your shopping.</a></li>
-	    </ul>
-	    </div>';
-	 	}
-  	}
-?>	
-<?php }break;}?>
 </body>
 </html>
