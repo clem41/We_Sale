4ae709d85acbb20eb0cfad2d9e8b29ae899bc7d3
Clotilde <clotilde.zenone@isen.yncrea.fr>
2019-12-06 21:46:59 +0100

    fixe a problem in the connexion page

--- index.php
+++ index.php
@@ -5,8 +5,9 @@ ini_set('session.use_only_cookies', 0); //是否仅用cookie
 ini_set('session.use_trans_sid', 1); //是否自动url带上session id
 ini_set('session.name', 'sid');
 
-session_id($_GET[session_name()]);
+
 if(isset($_GET[session_name()])){
+  session_id($_GET[session_name()]);
 session_start();
 }	
 
